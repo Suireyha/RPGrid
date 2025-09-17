8b8abb43e8bd01b8bd875fb3396d5ff53d4737f4
Suireyha <Marvink2296@gmail.com>
2025-09-17 19:30:34 +1000

    Added basic inventory.add method

--- src/Inventory.java
+++ src/Inventory.java
@@ -2,6 +2,11 @@ import java.util.ArrayList;
 
 public class Inventory{
     ArrayList<Item> contents = new ArrayList<>(); //Will actually contain the items
+    
+    public void add(Item item){ //Still undecided where items will actually be made may need to make a package of item presets?
+        contents.add(item); 
+    }
+
     Inventory(){
         //Inventory constructor
     }
