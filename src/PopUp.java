46c7459fc3bc8768d0cb84849e9c05e70377daad
Suireyha <Marvink2296@gmail.com>
2025-09-21 01:50:54 +1000

    Making a custom row div file for PopUp to manage content placement

--- src/PopUp.java
+++ src/PopUp.java
@@ -16,16 +16,17 @@ public class PopUp <T extends MapEntity> extends JFrame{
     JPanel container;
     JPanel infoPanel;
     JPanel btnPanel;
-    Color titleCol;
-    Color textCol = new Color(255, 255, 255); //White by default
+    Color uiHeader = new Color(255, 255, 191);
     T evoker;
 
     private void SetUp(){
         container = new JPanel();
+        
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //Close THIS popup, but don't destroy the whole program
         this.setContentPane(container);
         this.setVisible(true);
         this.setBounds(1000, 400, winWidth, winHeight); //Window is being drawn at x=350 y=50, dimensions are 1000^2
+        container.setLayout(new BorderLayout());
         container.setBackground(new Color(47, 48, 49));
         //popupContainer.setLayout(getLayout()); //Give me like a flex box kinda thing
 
@@ -34,10 +35,12 @@ public class PopUp <T extends MapEntity> extends JFrame{
 
         infoPanel.setBackground(new Color(47, 48, 49));
         infoPanel.setBorder(new EmptyBorder(0, 0, 80, 0)); //Padding on the bottom
+        infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS)); //Should let things flow onto new lines
+        
         btnPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
         btnPanel.setBackground(new Color(47, 48, 49));
         
-
+        
         container.add(infoPanel, BorderLayout.CENTER); //Justify infoPanel to fill
         container.add(btnPanel, BorderLayout.SOUTH); //Justify btnPanel to stick to the bottom
 
@@ -45,15 +48,23 @@ public class PopUp <T extends MapEntity> extends JFrame{
     
     PopUp(T entity){
         SetUp();
-
+        
         Btn atkBtn = new Btn(true, "ATK", 0);
         Btn invBtn = new Btn(true, "INV", 1);
         String titleText = entity.getName();
         TextHeaders title = new TextHeaders("~~~~~~ " + titleText + " ~~~~~~", TextHeaders.Header.HEADER1, entity.getNameTextCol());
         
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        infoPanel.add(title);
         
         switch(entity.getEntityType()){
             case PLAYER:
+                JPanel statsDiv = new JPanel();
+                TextHeaders statHeader = new TextHeaders("Stats:", TextHeaders.Header.HEADER1, uiHeader);
+                statHeader.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+                statsDiv.add(statHeader);
+                infoPanel.add(statsDiv);
                 break;
             case ENEMIE:
                 break;
@@ -65,9 +76,8 @@ public class PopUp <T extends MapEntity> extends JFrame{
         
 
         
-        title.setAlignmentX(Component.CENTER_ALIGNMENT);
-        infoPanel.add(title);
-        
+
+
         btnPanel.add(atkBtn);        
         btnPanel.add(invBtn);
     }
