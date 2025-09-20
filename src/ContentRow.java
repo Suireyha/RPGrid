eeac97d61cc74cd198c8def83de6a3b8285e0f60
Suireyha <Marvink2296@gmail.com>
2025-09-21 01:54:43 +1000

    Make the ContentRow.java constructor

--- src/ContentRow.java
+++ src/ContentRow.java
@@ -1,6 +1,12 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.*;
 
 public class ContentRow extends JPanel{
-
+    ContentRow(){
+        super();
+        this.setBackground(new Color(47, 48, 49));
+        this.setBorder(new EmptyBorder(0,0,0,0));
+        this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+    }
 }
